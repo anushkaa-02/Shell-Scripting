3516122660409f78e3f9d47ac3c8d9450cfaca89
Anushka Paul <87390353+pilipi-puu-puu@users.noreply.github.com>
2022-08-21 10:31:09 +0530

    Create Looping with numbers.sh

---
+++ Practice_Questions/Hackerrank/Looping with numbers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Author: Anushka Paul
+#Question: Use a for loop to display the natural numbers from 1 to 50.
+#Input Format : There is no input.
+#Output format : 1 2 3 ..50                                                         
+# Start #
+
+for i in {1..50}
+do 
+echo "$i"
+done 
+
+# End #
